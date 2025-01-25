deb3305267a241d8ddd9865fefd57962613ce04b
viole <violetraisondetre@gmai.com>
2025-01-25 17:36:35 +0900

    feat: 🎸 結果と一覧へ戻るボタンの画面を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:zenn_ai_hackathon_2501_frontend/screens/game_list_page.dart';
+import 'package:zenn_ai_hackathon_2501_frontend/pages/game_list_page.dart';
 
 Future main() async {
   try {
